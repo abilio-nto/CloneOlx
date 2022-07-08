e93d4f3de8fa637e1b4f0590f52aa7f7cd8cd7d6
abilio <abiliofcb@gmail.com>
2022-07-07 21:54:19 -0300

    PAgina de login iniciada

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import {Route,Routes} from 'react-router-dom'
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound"
+import Signin from './pages/Signin'
 
 const Rotas = () =>{
     return(
         <Routes>
            <Route path="/" element={<Home/>}/>
            <Route path="/About" element={<About/>}/>
+           <Route path="/signin" element={<Signin/>}/>
            <Route path="*" element={<NotFound/>}/>
         </Routes>
     )
